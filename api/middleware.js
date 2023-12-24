45f66e785ef8490b4c8db3098b273afc6e9c23fe
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2023-12-24 15:52:18 +0100

    Load balancer implemented in middleware

--- api/middleware.js
+++ api/middleware.js
@@ -1,6 +1,26 @@
 
 
+const loadBalancer = {
+    users: {
+        services: ['usersService1', 'usersService2', 'usersService3'],
+        index: 0
+    },
+    articles: {
+        services: ['articlesService1', 'articlesService2', 'articlesService3'],
+        index: 0
+    }
+}
+
+
 export const balancerMiddleware = async (req, res, next) => {
-    console.log(req.url);
+    //console.log(req.baseUrl);
+    let url = req.baseUrl;
+    let requestedService = loadBalancer?.[url.substring(1)];
+    if(requestedService){
+        requestedService.index = requestedService.index < requestedService.services.length ? requestedService.index : 0;
+        let service = requestedService.services[requestedService.index];
+        requestedService.index += 1;
+        console.log(service);
+    }
     next();
 };
