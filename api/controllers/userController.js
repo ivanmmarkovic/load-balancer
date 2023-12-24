36d974ea4b29bfc8d528a6335d280875ce005894
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2023-12-24 18:56:14 +0100

    Addapt users controller

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,14 +1,26 @@
 
 import { returnService } from "../loadBalancer.js";
+import { createUserUtil, getUsersUtil } from "../utils.js";
 
 export const getUsers = async (req, res, next) => {
     let service = returnService(req);
+    // check if service is null
     console.log(service);
+    let {protocol, host, port} = service;
+    try {
+        let d = await getUsersUtil(protocol, host, port);
+        console.log(d);
+    } catch (error) {
+        console.log('Error ', error.message);
+    }
     return res.status(200).json({ message: 'Get users route' });
 };
 
 export const createUser = async (req, res, next) => {
     let service = returnService(req);
-    console.log(service);
+    // check if service is null
+    let {protocol, host, port} = service;
+    let o = await createUserUtil(protocol, host, port, req.body);
+    console.log(o);
     return res.status(200).json({ message: 'Create user route' });
 };
