0ad89a8b856037d8d17fa51f97903b16c218e53c
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2023-12-24 15:53:49 +0100

    Minor index.js fix

--- api/index.js
+++ api/index.js
@@ -1,14 +1,19 @@
 
 import express from 'express';
+import { balancerMiddleware } from './middleware.js';
 
 const app = express();
 
+import { router as userRouter } from './routes/userRoutes.js';
+import { router as articleRouter } from './routes/articleRoutes.js';
 
-app.get('/', async (req, res, next) => {
+
+app.get('/', balancerMiddleware, async (req, res, next) => {
     return res.status(200).json({ message: 'Hi' });
 });
 
-console.log('Hi');
+app.use('/users', balancerMiddleware, userRouter);
+app.use('/articles', balancerMiddleware, articleRouter);
 
 
 app.listen(8080, () => console.log('Api service is listening on port 8080'));
