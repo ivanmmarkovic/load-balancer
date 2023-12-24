693817d90ff049f5fc4a5d55c98d7adc37a8543e
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2023-12-24 17:13:20 +0100

    Remove load balancer middleware anf fix index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
 
 import express from 'express';
-import { balancerMiddleware } from './middleware.js';
 
 const app = express();
 
@@ -8,12 +7,12 @@ import { router as userRouter } from './routes/userRoutes.js';
 import { router as articleRouter } from './routes/articleRoutes.js';
 
 
-app.get('/', balancerMiddleware, async (req, res, next) => {
+app.get('/', async (req, res, next) => {
     return res.status(200).json({ message: 'Hi' });
 });
 
-app.use('/users', balancerMiddleware, userRouter);
-app.use('/articles', balancerMiddleware, articleRouter);
+app.use('/users', userRouter);
+app.use('/articles', articleRouter);
 
 
 app.listen(8080, () => console.log('Api service is listening on port 8080'));
